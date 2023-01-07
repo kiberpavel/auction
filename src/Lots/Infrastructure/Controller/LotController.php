2bee23eb79f30f83f09e6d7aa1da7b52d94c7ec3
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-01-07 23:19:20 +0200

    Add services for handle lot operations and add endpoints

--- src/Lots/Infrastructure/Controller/LotController.php
+++ src/Lots/Infrastructure/Controller/LotController.php
@@ -3,13 +3,20 @@
 namespace App\Lots\Infrastructure\Controller;
 
 use App\Lots\Infrastructure\Service\LotCreation;
+use App\Lots\Infrastructure\Service\LotEdit;
+use App\Lots\Infrastructure\Service\LotRemove;
+use App\Lots\Infrastructure\Service\LotsList;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LotController
 {
-    public function __construct(private readonly LotCreation $lotCreation)
+    public function __construct(
+        private readonly LotCreation $lotCreation,
+        private readonly LotRemove $lotRemove,
+        private readonly LotEdit $lotEdit,
+        private readonly LotsList $lotsList)
     {
     }
 
@@ -17,5 +24,23 @@ class LotController
     public function addNewLot(Request $request): JsonResponse
     {
         return $this->lotCreation->createLot($request);
+    }
+
+    #[Route('api/lot/delete', methods: ['POST'])]
+    public function removeLot(Request $request): JsonResponse
+    {
+        return $this->lotRemove->removeLot($request);
+    }
+
+    #[Route('api/lot/update', methods: ['POST'])]
+    public function updateLot(Request $request): JsonResponse
+    {
+        return $this->lotEdit->editLot($request);
+    }
+
+    #[Route('api/lot/list', methods: ['GET'])]
+    public function listOfLots(): JsonResponse
+    {
+        return $this->lotsList->outputLots();
     }
 }
