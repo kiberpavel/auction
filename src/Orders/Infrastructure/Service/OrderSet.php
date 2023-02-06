22b7f5e5d2dce19c0930eef0d0fd423df00d2c81
Butenko Pavel <pavlo.butenko@nixsolutions.com>
2023-02-06 13:34:04 +0200

    Update set order service

--- src/Orders/Infrastructure/Service/OrderSet.php
+++ src/Orders/Infrastructure/Service/OrderSet.php
@@ -5,6 +5,7 @@ namespace App\Orders\Infrastructure\Service;
 use App\Orders\Domain\Factory\OrderFactory;
 use App\Orders\Domain\Repository\OrderRepositoryInterface;
 use App\Orders\Infrastructure\Adapters\BuyerAdapter;
+use App\Orders\Infrastructure\Adapters\LotAdapter;
 use App\Shared\Infrastructure\Helper\ResponseMessage;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,19 +15,26 @@ class OrderSet
     public function __construct(
         private readonly OrderRepositoryInterface $orderRepository,
         private readonly OrderFactory $orderFactory,
-        private readonly BuyerAdapter $buyerAdapter)
+        private readonly BuyerAdapter $buyerAdapter,
+        private readonly LotAdapter $lotAdapter)
     {
     }
 
     public function addOrder(Request $request): JsonResponse
     {
-        $buyerId = $request->get('buyerId');
+        $lotId = $request->get('lotId');
 
-        if (!$buyerId) {
+        if (!$lotId) {
             return new JsonResponse([
                 'error' => ResponseMessage::PARAMS_ERROR,
             ], 400);
         }
+
+        $lot = $this->lotAdapter->importLot($lotId);
+
+        $apiBuyer = $this->buyerAdapter->importBuyerByLot($lot);
+
+        $buyerId = $apiBuyer[0]->getId();
 
         $buyer = $this->buyerAdapter->importBuyer($buyerId);
 
@@ -39,6 +47,7 @@ class OrderSet
         try {
             $order = $this->orderFactory->create($buyer);
             $this->orderRepository->add($order);
+            $this->lotAdapter->updateStatus($lotId);
         } catch (\TypeError $error) {
             return new JsonResponse([
                 'error' => $error->getMessage(),
